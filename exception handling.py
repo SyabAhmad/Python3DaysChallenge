ff22a1d2bfc638b66c46a5793271e319bbc9f11a
Syed Syab Ahmad Shah <81256221+SyabAhmad@users.noreply.github.com>
2023-03-01 21:30:37 +0500

    day 3 in PYTHON

---
+++ exception handling.py
@@ -0,0 +1,6 @@
+print("Exception handling in Python")
+
+try:
+    print("de Develiper" + 12)
+except:
+    print(" some thing went wrong")
---
+++ math in python.py
@@ -0,0 +1,13 @@
+print("Math Functions in python")
+
+# to find min or mix Value
+print(min(12, 23, 1102))
+print(max(21, 23, 0.4, 543))
+
+# to find absolute value
+print(abs(123))
+print(abs(-123))
+
+# to take Power of the value
+print(pow(2, 2))
+print(pow(2, -2))
